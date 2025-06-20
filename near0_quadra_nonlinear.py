85c3642895c651292a8bf13c0469cc9377e2090f
Gamal-on <garance.boudot@gmail.com>
2025-06-20 16:35:20 +0200

    yip: hourra

---
+++ near0_quadra_nonlinear.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tools
+import constants as cnt
+import scipy.optimize as opt
+
+# Fit function
+
+
+def fit_func_quadra(x, beta, gamma, n, E, alpha):
+    """Fit function for the nonlinear analysis of C/T - C_schottky vs T².
+    Parameters: x = T² (K²), beta = mJ/K⁴.mol, gamma = mJ/K².mol, n = dimensionless"""
+    phonon = beta * x
+    y = E/(cnt.k*np.sqrt(x))
+    schottky = (y**2) * np.exp(y)/((np.exp(y) + 1)**2)
+    quadra = alpha*(x**2)
+    return phonon + gamma + n*cnt.r*1e3*schottky/np.sqrt(x) + quadra
+
+# Non linear fit
+
+
+def nonlinear_fit_quadra(a, b, x_carre, y, bounds):
+    """Non linear fit usinf curve fit from scipy library : 
+    a, b = sclars (bounds), 
+    x_carre = array
+    y = array
+    err_y = array
+    bounds = bounds (2-tuple of arrays-like)"""
+    x_carre_interval, y_interval = tools.tab_interval(x_carre, y, a, b)
+    fit = opt.curve_fit(fit_func_quadra, x_carre_interval, y_interval, bounds=bounds,
+                        absolute_sigma=True)
+    return fit[0]
+
+
+def plot_fit_quadra(a, b, x_carre, y, bounds):
+    """Plotting data and non linear fit usinf curve fit from scipy library : 
+    a, b = sclars (bounds), 
+    x_carre = array
+    y = array
+    err_y = array
+    bounds = bounds (2-tuple of arrays-like)"""
+    x_carre_interval, y_interval = tools.tab_interval(x_carre, y, a, b)
+    beta, gamma, n, E, alpha = nonlinear_fit_quadra(
+        a, b, x_carre, y, bounds)
+    print("Beta, Gamma, n, E, alpha : ", beta, gamma, n, E, alpha)
+    plt.figure()
+    plt.plot(x_carre_interval, y_interval, "g.", label="Experimental")
+    plt.plot(x_carre_interval, fit_func_quadra(
+        x_carre_interval, beta, gamma, n, E, alpha), "c-", label="Fit")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
+def main():
+    plot_fit_quadra(0, 100, cnt.squared_temperature,
+                    cnt.C_div_T, ([0.1, 0, 5e-3, 9.3e-23, -1], [1, 5, 1.1e-2, 1.2e-22, 1]))
+
+
+if __name__ == "__main__":
+    main()
