697d861526df5ae9c2b63c39ba1846f43997d2d6
Gamal-on <garance.boudot@gmail.com>
2025-06-24 12:12:03 +0200

    yip

--- debye_integration.py
+++ debye_integration.py
@@ -42,23 +42,45 @@ def progressive_integration(x, y):
 
 def debye_integral(temperature):
     """Return a tuple of array-like objet : the chosen interval with the debye temperature and the integration"""
-    debye_temperature = np.arange(0.1, 700, 0.1)
-    y = fonction_debye(temperature, debye_temperature)
-    integral = progressive_integration(debye_temperature, y)
-    return debye_temperature, integral
+    tab_debye_temperature = np.arange(0.01, 600, 0.01)
+    y = fonction_debye(temperature, tab_debye_temperature)
+    integral = progressive_integration(tab_debye_temperature, y)
+    return tab_debye_temperature, integral
 
 # Step 3 : Calculate the theorical beta and test it
 
 
 def tab_beta(temperature):
     """Return the beta value, function of the temperature, for several possible Debye temperatures"""
-    debye_temperature, integral = debye_integral(temperature)
-    tab_beta = integral/(debye_temperature**3)
-    return debye_temperature, 9*cnt.N*cnt.k*tab_beta
+    tab_debye_temperature, integral = debye_integral(temperature)
+    tab_beta = integral/(tab_debye_temperature**3)
+    return tab_debye_temperature, 9*cnt.N*cnt.k*tab_beta
+
+
+def find_closest(alpha, arr):
+    closest_value = arr[0]
+    closest_index = 0
+    min_diff = abs(alpha - closest_value)
+    for i in range(1, len(arr)):
+        value = arr[i]
+        diff = abs(alpha - value)
+        if diff < min_diff:
+            min_diff = diff
+            closest_value = value
+            closest_index = i
+    return closest_value, closest_index
+
+# Step 4 : Fin the debye temperature
+
+
+def debye_temperature_integration(beta_exp, temperature):
+    tab_debye_temperature, tab_possible_beta = tab_beta(temperature)
+    closest_value, closest_idx = find_closest(beta_exp, tab_possible_beta)
+    return tab_debye_temperature[closest_idx], closest_value
 
 
 def main():
-    print(tab_beta(10)[1])
+    print(debye_temperature_integration(0.46e-3, 20))
 
 
 if __name__ == "__main__":
