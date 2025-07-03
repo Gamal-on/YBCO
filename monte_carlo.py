2204191588400a4e9b2fa84610bfc777e6f8c44c
Gamal-on <garance.boudot@gmail.com>
2025-07-03 11:55:58 +0200

    yip

--- monte_carlo.py
+++ monte_carlo.py
@@ -25,6 +25,12 @@ def chi2_one_iteration(x_data, y_data, f, bounds, size_params):
 
 
 def minimize_chi2(x_data, y_data, f, N, bounds, size_params):
+    """Optimisation using the minization of chi2
+    x_data, y_data : arrays-like
+    f : model
+    N : number of iterations, integer
+    bounds : tuple of array-like
+    size_params : integers, number of parameters"""
     i = 0
     chi2_ini = 1e6
     params_opt = np.ones(size_params)
@@ -39,11 +45,14 @@ def minimize_chi2(x_data, y_data, f, N, bounds, size_params):
     return chi2_ini, params_opt
 
 
+def ajustement_mc(x_data, y_data, f, a, b, N, bounds, size_params):
+    x_interval, y_interval = tools.tab_interval(x_data, y_data, a, b)
+    return minimize_chi2(x_interval, y_interval, f, N, bounds, size_params)
+
+
 def main():
-    temperature_squared_bounded, hc_div_temp_bounded = tools.tab_interval(
-        cnt.squared_temperature_HPHT, cnt.hc_div_temp_HPHT, 36, 144)
-    print(minimize_chi2(temperature_squared_bounded,
-          hc_div_temp_bounded, model_linear, 1000, ([0.4, 0.5], [0, 10]), 2))
+    print(ajustement_mc(cnt.squared_temperature_HPHT, cnt.hc_div_temp_HPHT,
+          model_linear, 36, 144, 1e5, ([0.4, 0.5], [0, 10]), 2))
 
 
 if __name__ == "__main__":
